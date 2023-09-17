3e8cec58b075d03f29b1614d94debb08ecb064ea
b_nounours <b_nounours@hotmail.com>
2023-09-17 23:30:32 +0200

    Power Management rely on Kodi command and send notification to Kodi

--- x735-pwr.sh
+++ x735-pwr.sh
@@ -10,7 +10,7 @@ echo "$BOOT" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$BOOT/direction
 echo "1" > /sys/class/gpio/gpio$BOOT/value
 
-echo "Your device are shutting down..."
+echo "Power Management started..."
 
 while [ 1 ]; do
   shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
@@ -22,14 +22,17 @@ while [ 1 ]; do
       /bin/sleep 0.02
       if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
         echo "Your device are shutting down", SHUTDOWN, ", halting Rpi ..."
-        sudo poweroff
+        sync
+        kodi-send --host=127.0.0.1 -a "Notification(Power Button, Shutdown required,10000)"
+        kodi-send --host=127.0.0.1 -a "Powerdown"
         exit
       fi
       shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
     done
     if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then
       echo "Your device are rebooting", SHUTDOWN, ", recycling Rpi ..."
-      sudo reboot
+      kodi-send --host=127.0.0.1 -a "Notification(Power Button, Reboot required,10000)"
+      kodi-send --host=127.0.0.1 -a "Reboot"
       exit
     fi
   fi
